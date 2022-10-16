74628fb9ef4edd12e2fe16b464233e45628e9e41
Howard Wang <howard122696@gmail.com>
2022-10-15 21:55:28 -0700

    clear inputs

--- src/Components/Searchbar.jsx
+++ src/Components/Searchbar.jsx
@@ -1,29 +1,48 @@
 import { useState } from 'react'
 import './Searchbar.css'
 import SearchIcon from '@mui/icons-material/Search';
+import CloseIcon from '@mui/icons-material/Close';
 
 const Searchbar = ({placeholder, data}) => {
   const [filteredData, setFilteredData] = useState([])
+  const [wordEntered, setWordEntered] = useState('')
 
   const handleFilter = (e) => {
     const searchWord = e.target.value
+    setWordEntered(searchWord)
     const newFilter = data.filter((value) => {
       return value.title.toLowerCase().includes(searchWord.toLowerCase())
     })
-    setFilteredData(newFilter)
+
+    if(searchWord === ''){
+      setFilteredData([])
+    } else {
+      setFilteredData(newFilter)
+    }
+  }
+
+  const clearInput = () => {
+    setFilteredData([])
+    setWordEntered('')
   }
 
   return (
     <div className='search'>
       <div className="searchInputs">
-        <input type="text" placeholder={placeholder} onChange={handleFilter}/>
+        <input 
+          type="text" 
+          value={wordEntered} 
+          placeholder={placeholder} 
+          onChange={handleFilter}
+        />
         <div className='searchIcon'>
-          <SearchIcon />
+          {filteredData.length === 0 ? (<SearchIcon />) : 
+          (<CloseIcon id='clearBtn' onClick={clearInput}/>)}
         </div>
       </div>
       { filteredData.length != 0 && (
        <div className="dataResult">
-          {filteredData.map((value, key) => {
+          {filteredData.slice(0, 15).map((value, key) => {
             return (
               <a className='dataItem' href='#'> 
                 <p>{value.title}</p> 
